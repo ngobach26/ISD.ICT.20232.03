18902bd1ab0bd8b6edd0cc0117c758b4ce2d5156
Thanh-Cong-BH <thanhcongbh1502@gmail.com>
2024-05-27 11:10:54 +0700

    Fix: missing controller, fxml, handler files

--- src/utils/Configs.java
+++ src/utils/Configs.java
@@ -15,6 +15,8 @@ public class Configs {
 	public static final String IMAGE_PATH = "assets/images";
 	public static final String POPUP_PATH = "/views/fxml/popup.fxml";
 	public static final String SPLASH_SCREEN_PATH = "/views/fxml/splash.fxml";
+	public static final String INVOICE_SCREEN_PATH = "/views/fxml/invoice.fxml";
+	public static final String INVOICE_MEDIA_SCREEN_PATH = "/views/fxml/media_invoice.fxml";
 	
 	
 	public static float PERCENT_VAT = 10;
@@ -25,5 +27,14 @@ public class Configs {
 	
 	//	FONT
 	public static Font REGULAR_FONT = Font.font("Segoe UI", FontWeight.NORMAL, FontPosture.REGULAR, 24);
+	
+	public static String[] PROVINCES = { "Bắc Giang", "Bắc Kạn", "Cao Bằng", "Hà Giang", "Lạng Sơn", "Phú Thọ",
+			"Quảng Ninh", "Thái Nguyên", "Tuyên Quang", "Yên Bái", "Điện Biên", "Hòa Bình", "Lai Châu", "Sơn La",
+			"Bắc Ninh", "Hà Nam", "Hải Dương", "Hưng Yên", "Nam Định", "Ninh Bình", "Thái Bình", "Vĩnh Phúc", "Hà Nội",
+			"Hải Phòng", "Hà Tĩnh", "Nghệ An", "Quảng Bình", "Quảng Trị", "Thanh Hóa", "Thừa Thiên-Huế", "Đắk Lắk",
+			"Đắk Nông", "Gia Lai", "Kon Tum", "Lâm Đồng", "Bình Định", "Bình Thuận", "Khánh Hòa", "Ninh Thuận",
+			"Phú Yên", "Quảng Nam", "Quảng Ngãi", "Đà Nẵng", "Bà Rịa-Vũng Tàu", "Bình Dương", "Bình Phước", "Đồng Nai",
+			"Tây Ninh", "Hồ Chí Minh", "An Giang", "Bạc Liêu", "Bến Tre", "Cà Mau", "Đồng Tháp", "Hậu Giang",
+			"Kiên Giang", "Long An", "Sóc Trăng", "Tiền Giang", "Trà Vinh", "Vĩnh Long", "Cần Thơ" };
 
 }
