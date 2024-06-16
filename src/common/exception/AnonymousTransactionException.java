1dd69870c85a2c14dab6ba89e4b10eb63a63dbb8
moctran <87555673+moctran@users.noreply.github.com>
2024-06-16 17:59:42 +0700

    Add files via upload

--- src/common/exception/AnonymousTransactionException.java
+++ src/common/exception/AnonymousTransactionException.java
@@ -1,7 +1,5 @@
 package common.exception;
 
-;
-
 public class AnonymousTransactionException extends PaymentException {
     public AnonymousTransactionException() {
         super("ERROR: Giao dịch bị nghi ngờ gian lận");
